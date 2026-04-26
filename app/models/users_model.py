b1be9fc0398d857576badc0effdd6f53f3fc7d45
vanika02 <vanikadangi02@gmail.com>
2026-04-26 15:50:07 +0000

    user model done

--- app/models/users_model.py
+++ app/models/users_model.py
@@ -2,3 +2,16 @@ from sqlalchemy import Column, String, Integer, DateTime
 from datetime import datatime
 
 from app.db import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    username = Column(String, unique=True, nullable=False, index=True)
+
+    email = Column(String, unique=True, nullable=False, index=True)
+
+    hashed_password = Column(String, nullable=False)
+
+    created_at = Column(DateTime, default=datatime.utcnow)
