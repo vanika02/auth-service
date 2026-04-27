2b7742e6a3b4893972213a5e02709a238fdddf72
vanika02 <vanikadangi02@gmail.com>
2026-04-27 15:45:31 +0000

    verify hash password func created

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -14,3 +14,12 @@ pwd_context = CryptContext(
 
 def hashed_password(password: str) -> str:
     return pwd_context.hash(password)
+
+def verify_password(
+    plain_password: str,
+    hashed_password: str  
+) -> bool:
+    return pwd_context.verify(
+        plain_password,
+        hashed_password
+    )
