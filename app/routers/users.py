d4cb2ece94933e3e385e3ce924d323180074f0a4
vanika02 <vanikadangi02@gmail.com>
2026-04-28 15:26:17 +0000

    user.py router

--- app/routers/users.py
+++ app/routers/users.py
@@ -3,3 +3,16 @@ from sqlalchemy.orm import Session
 
 from app.db.db import get_db
 from app.models.users_model import User
+from app.schemas.user_schema import (
+    UserCreate,
+    UserLogin,
+    UserResponse
+)
+from app.auth.auth import (
+    hash_password,
+    verify_password,
+    created_access_token,
+    decode_access_token
+)
+
+router = APIRouter(prefix="/users", tags=["users"])
