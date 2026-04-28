15b7e832834b218f31de54101e9227e5acb3fdcd
vanika02 <vanikadangi02@gmail.com>
2026-04-28 15:47:40 +0000

    login func created

--- app/routers/users.py
+++ app/routers/users.py
@@ -40,3 +40,31 @@ def signup(user: UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return {"message": "User created successfully"}
+
+
+@router.post("/login")
+def login(user: UserLogin, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if not db_user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid credentials"
+        )
+    
+    if not verify_password(user.password,  db_user.hashed_password):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid credentials"
+        )
+
+    token = created_access_token(
+        {"sub": db_user.email}
+    )
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
