5c5ceb325825622fa1f9e09e6ec684623aee96d7
vanika02 <vanikadangi02@gmail.com>
2026-05-01 15:52:08 +0000

    .env fil created

--- app/db/db.py
+++ app/db/db.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://authuser:strongpassword@localhost/authdb" 
+DATABASE_URL = os.getenv("DATABASE_URL") 
 
 engine = create_engine(DATABASE_URL)
 
